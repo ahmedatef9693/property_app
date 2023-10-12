e74d75c8949c7f87b4ba4d9f9a4fc8b21cd8bd89
ahmedatef9693 <ahmedatef93legend@gmail.com>
2023-10-12 18:24:16 +0300

    post and pre sync

--- estate_app/estate_app/doctype/property/patches/property_patchv2.py
+++ estate_app/estate_app/doctype/property/patches/property_patchv2.py
@@ -7,6 +7,6 @@ def execute():
     properties = frappe.db.get_all('Property',fields = ['name',"city"])
     for prop in properties:
         if prop.city == "Bushfort":
-            frappe.db.set_value("Property",prop.name,"is_sold","1")
+            frappe.db.set_value("Property",prop.name,"is_sold","1",update_modified=False)
 
 
