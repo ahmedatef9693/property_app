d3f4e80122d29d71fafbd20e2fc26259a5c6a3a8
ahmedatef9693 <ahmedatef93legend@gmail.com>
2023-10-12 18:09:22 +0300

    patches updates

---
+++ estate_app/estate_app/doctype/property/patches/property_patch.py
@@ -0,0 +1,12 @@
+import frappe
+
+
+
+
+def execute():
+    properties = frappe.db.get_all('Property',fields = ['name',"city"])
+    for prop in properties:
+        if prop.city == "Bushfort":
+            frappe.db.set_value("Property",prop.name,"is_sold","1")
+
+
